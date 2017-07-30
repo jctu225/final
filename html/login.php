3ee9ef88412bebba46b3abf36a8e2f99f36bba77
Joshua <jctucker225@gmail.com>
2017-07-29 20:05:05 -0400

    adding in html/php and css

---
+++ html/login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML>
+<html>
+<header>
+</header>
+
+<body>
+ <div class="container">
+  <div class="row">
+   <div class="col">
+    <div class="panel panel-default">
+     <div class="e">
+      <p> Hey from the panel!!1! </p>
+     </div>
+    </div>
+   </div>
+
+
+
+
+
+  
+ </div>
+</body>
+<link rel="stylesheet/scss" type="text/css" href="master_css.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+</html>
