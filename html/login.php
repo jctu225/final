859af6229aca87a9939e4d3ffeaa9b1f3805450b
Joshua <jctucker225@gmail.com>
2017-07-31 09:42:18 -0400

    initial html/css/php built

--- html/login.php
+++ html/login.php
@@ -1,27 +1,43 @@
 <!DOCTYPE HTML>
 <html>
-<header>
-</header>
+<head>
+
+<link type='text/css' rel='stylesheet' href='style.php'>
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" 
+        integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" 
+        crossorigin="anonymous">
+</script>
+<link rel="stylesheet" 
+      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" 
+      integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" 
+      crossorigin="anonymous">
+<link href="https://fonts.googleapis.com/css?family=Zilla+Slab" rel="stylesheet">
+
+</head>
 
 <body>
+
+<?php
+$css = '<style>'.file_get_contents('style.css').'</style>';
+echo $css;
+?>
  <div class="container">
   <div class="row">
-   <div class="col">
-    <div class="panel panel-default">
-     <div class="e">
-      <p> Hey from the panel!!1! </p>
-     </div>
-    </div>
+   <div class="title">
+    <h1>Welcome to Learnly</h1>
+    <p>The learning site.</p>
    </div>
-
-
-
-
-
-  
+  </div>
+    <div class="row">
+      <div class="blocks">
+       <div class="title">
+        <p>swag</p>
+       </div>
+      </div>
+    </div>
  </div>
+  
 </body>
-<link rel="stylesheet/scss" type="text/css" href="master_css.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+
 </html>
