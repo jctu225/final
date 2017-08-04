4ee6e6510504a325ee3067358407568a0c5153cb
Joshua <jctucker225@gmail.com>
2017-08-04 09:25:06 -0400

    adding more styling and profile page.

--- html/profile.php
+++ html/profile.php
@@ -1,15 +1,35 @@
 <!DOCTYPE HTML>
 <html>
 <head>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" 
+        integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" 
+        crossorigin="anonymous">
+ </script>
+ <link rel="stylesheet" 
+      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" 
+      integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" 
+      crossorigin="anonymous">
+ <link href="https://fonts.googleapis.com/css?family=Zilla+Slab" rel="stylesheet">
 </head>
 <body>
 
+
+
 <?php
-$css = '<style>'.file_get_contents('style.css').'</style>';
+$css = '<style>'.file_get_contents('styleing.css').'</style>';
 echo $css;
 ?>
-<div class="text">
-<h1>Welcome Back !</h1>
+<div class="container-fluid">
+
+ <div class="row this">
+  <div class="text">
+   <h1> Welcome Back! </h1>  
+  </div>
+ </div>
+
+
+
+</div>
 
 </body>
 </html>
